b29a445e579be1bc552cf861d3f3907c59382a74
Moises Linares <moiseslinares@live.com.mx>
2016-04-12 20:03:51 -0500

    Clase Principal

---
+++ github/src/main/java/com/test/git/github/main/Principal.java
@@ -0,0 +1,17 @@
+package com.test.git.github.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.test.git.github.model.OtroModel;
+
+public class Principal {
+
+	public void display(){
+		List<OtroModel> otros = new ArrayList<OtroModel>();
+		for (OtroModel otroModel : otros) {
+			System.out.println("Imprimir algo");
+		}
+	}
+	
+}
