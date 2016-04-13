812227d29882336ecd7478d59b337001f646f4a0
Moises Linares <blade_corp@hotmail.com>
2016-04-12 20:20:31 -0500

    Sumando en el metodo

--- github/src/main/java/com/test/git/github/main/Principal.java
+++ github/src/main/java/com/test/git/github/main/Principal.java
@@ -6,11 +6,18 @@ import java.util.List;
 import com.test.git.github.model.OtroModel;
 
 public class Principal {
-
-	public void display(){
+	/**
+	 * Comentario para el método
+	 * @param sum
+	 */
+	public void display(int sum){
+		
 		List<OtroModel> otros = new ArrayList<OtroModel>();
+		int i=0;
 		for (OtroModel otroModel : otros) {
 			System.out.println("Imprimir algo");
+			i++;
+			sum+=i;
 		}
 	}
 	
