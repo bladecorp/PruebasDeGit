2d4d64c0839ca3abf024f4a557b7b7e0769eb36d
Moises Linares <blade_corp@hotmail.com>
2016-04-12 20:13:10 -0500

    Cambios en Principal

--- github/src/main/java/com/test/git/github/main/Principal.java
+++ github/src/main/java/com/test/git/github/main/Principal.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.test.git.github.model.OtroModel;
+import com.test.git.github.model.PrimeraClase;
 
 public class Principal {
 
 	public void display(){
-		List<OtroModel> otros = new ArrayList<OtroModel>();
-		for (OtroModel otroModel : otros) {
-			System.out.println("Imprimir algo");
+		List<PrimeraClase> primeras = new ArrayList<PrimeraClase>();
+		System.out.println("Intermedio");
+		for (PrimeraClase primeraClase : primeras) {
+			
+			System.out.println("Impresion");
 		}
 	}
 	
